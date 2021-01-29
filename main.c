d35ab40d07dfec01821bd4ecfd37f6934ed4a7f2
hirakumorishima02 <business.ujinchu@gmail.com>
2021-01-29 11:16:39 +0900

    first sugoroku

--- main.c
+++ main.c
@@ -2,18 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void) {
-    int goal=30,dise=0;
+void init_rand();
 
-    srand((unsigned int)time(NULL));
+int main(void) {
+    int goal=30,dise1,dise2,dise3,num;
+
+    init_rand();
 
     while(goal>=0){
-        dise = 1 + rand() % 3;
-        goal -= dise;
-        printf("%d", dise);
+        puts("ダイスをいくつ振る?");
+        scanf("%d", &num);
+
+        if(num==1){
+            dise1 = 1 + rand() % 3;
+            goal -= dise1;
+        } else if(num==2){
+            dise1 = 1 + rand() % 3;
+            dise2 = 1 + rand() % 3;
+            goal -= dise1;
+            goal -= dise2;
+        } else{
+            dise1 = 1 + rand() % 3;
+            dise2 = 1 + rand() % 3;
+            dise3 = 1 + rand() % 3;
+            goal -= dise1;
+            goal -= dise2;
+            goal -= dise3;
+        }
+
+        printf("ゴールまであと…%d\n", goal);
     }
 
     puts("goal!");
 
     return 0;
+}
+
+void init_rand(){
+    srand((unsigned int)time(NULL));
 }
